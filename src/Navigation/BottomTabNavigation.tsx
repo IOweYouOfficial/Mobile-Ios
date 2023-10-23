4ebd635584c03f1a09600b38efee1b68b94a422a
omar jandali <omarjandali93@gmail.com>
2023-10-23 10:17:49 -0700

    Omar Jandali: Added initial Bottom Tab Navigation to the app

---
+++ src/Navigation/BottomTabNavigation.tsx
@@ -0,0 +1,127 @@
+/* eslint-disable prettier/prettier */
+import React, {useEffect, useState} from 'react';
+import {Platform} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Feather from 'react-native-vector-icons/Feather';
+import ScanScreen from '../Screens/Scan/ScanScreen';
+import AccountScreen from '../Screens/Account/AccountScreen';
+import ProfileScreen from '../Screens/Profile/ProfileScreen';
+import ActivityScreen from '../Screens/Activity/ActivityScreen';
+
+const BottomTabNavigation = () => {
+  const Tab = createBottomTabNavigator();
+
+  const [platformType, setPlatformType] = useState('Phone');
+  const [modelType, setModelType] = useState('iOS');
+
+  useEffect(() => {
+    determineDeviceType();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const determineDeviceType = () => {
+    Platform.OS === 'ios'
+      ? Platform.isPad
+        ? setPlatformType('Tablet')
+        : setPlatformType('Phone')
+      : setModelType('Android');
+    console.log(modelType);
+  };
+
+  const phoneNavigation = () => {
+    return (
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Screen
+          name="Home"
+          key="Home"
+          component={ScanScreen}
+          options={{
+            tabBarShowLabel: false,
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBarIcon: ({size, color}) => (<Feather name={'home'} color={color} size={size} />),
+          }}/>
+        <Tab.Screen
+          name="Account"
+          key="Account"
+          component={AccountScreen}
+          options={{
+            tabBarShowLabel: false,
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBarIcon: ({size, color}) => (<Feather name={'home'} color={color} size={size} />),
+          }}/>
+        <Tab.Screen
+          name="Activity"
+          key="Activity"
+          component={ActivityScreen}
+          options={{
+            tabBarShowLabel: false,
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBarIcon: ({size, color}) => (<Feather name={'home'} color={color} size={size} />),
+          }}/>
+        <Tab.Screen
+          name="Profile"
+          key="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarShowLabel: false,
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBarIcon: ({size, color}) => (<Feather name={'home'} color={color} size={size} />),
+          }}/>
+      </Tab.Navigator>
+    );
+  };
+
+  const tabletNavigation = () => {
+    return (
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Screen
+          name="Home"
+          key="Home"
+          component={ScanScreen}
+          options={{
+            tabBarShowLabel: false,
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBarIcon: ({size, color}) => (<Feather name={'home'} color={color} size={size} />),
+          }}/>
+        <Tab.Screen
+          name="Account"
+          key="Account"
+          component={AccountScreen}
+          options={{
+            tabBarShowLabel: false,
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBarIcon: ({size, color}) => (<Feather name={'home'} color={color} size={size} />),
+          }}/>
+        <Tab.Screen
+          name="Activity"
+          key="Activity"
+          component={ActivityScreen}
+          options={{
+            tabBarShowLabel: false,
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBarIcon: ({size, color}) => (<Feather name={'home'} color={color} size={size} />),
+          }}/>
+        <Tab.Screen
+          name="Profile"
+          key="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarShowLabel: false,
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBarIcon: ({size, color}) => (<Feather name={'home'} color={color} size={size} />),
+          }}/>
+      </Tab.Navigator>
+    );
+  };
+
+  return (
+    <NavigationContainer>
+      {
+        modelType === 'ios' ? platformType === 'Tablet' ? tabletNavigation() : phoneNavigation() : phoneNavigation()
+      }
+    </NavigationContainer>
+  );
+};
+
+export default BottomTabNavigation;
